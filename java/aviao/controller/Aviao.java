4df675f27ed1699a692bc5c3db633b2ecd7c5d43
Wall <31081097+megacomputerman@users.noreply.github.com>
2022-05-18 23:21:46 -0400

    Added aviao attributes

--- java/aviao/controller/Aviao.java
+++ java/aviao/controller/Aviao.java
@@ -1,7 +1,26 @@
 package aviao.controller;
 
 public class Aviao { 
+    private String modelo;
+    private String identificador;
+    private boolean motor;
+    private float altura;
+    
+    public void setModelo( String modelo ){
+        this.modelo = modelo; 
+    }
 
+    public void setIdentificador( String identificador ){
+        this.identificador = identificador; 
+    }
+    
+    public void setMotor( boolean motor ){
+        this.motor = motor; 
+    }
+    
+    public void setAltura( float altura ){
+        this.altura = altura; 
+    }
     public void imprimirOk() {
         System.out.println("Ok");
     }
