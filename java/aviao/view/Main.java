39b336d63bc6caabd37d40b3f376812d97607b95
Wall <31081097+megacomputerman@users.noreply.github.com>
2022-05-20 12:30:46 -0400

    Update Main.java

--- java/aviao/view/Main.java
+++ java/aviao/view/Main.java
@@ -4,9 +4,32 @@ import aviao.controller.Aviao;
 
 public class Main { 
     public static void main(String[] args) {
-       // System.out.println("Hello World !!");
-       new Aviao().imprimirOk();
-      //imprimirOk();
+      // System.out.println("Hello World !!");
+     
+      // Etapa 1	   
+      Aviao aviao = new Aviao(); 
+      aviao.imprimirOk();
+
+      // Etapa 2
+      //System.out.println("Modelo: " + aviao.getModelo() + " Identificador: " + aviao.getIdentificador());
+      addAviao(aviao, "wall", "goku", true, 1000);
+      printAviao(aviao);
+
     }
+
+   public static void printAviao( Aviao aviao ){
+       System.out.println("Modelo: " + aviao.getModelo());
+       System.out.println("Identificador: " + aviao.getIdentificador());
+       System.out.println("Motor: " + aviao.getMotor());
+       System.out.println("Altura: " + aviao.getAltura());
+   }
+
+   public static void addAviao( Aviao aviao, String modelo, String id, boolean motor, float altura ){
+   	aviao.setModelo(modelo);
+        aviao.setIdentificador(id);
+        aviao.setMotor(motor);
+        aviao.setAltura(altura);
+   }
+
 }
 
