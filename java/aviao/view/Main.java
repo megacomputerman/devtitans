65059cf84d4eb8b1fe77aabd991d84457d1fc0b7
Wall <31081097+megacomputerman@users.noreply.github.com>
2022-05-20 19:47:46 -0400

    Update Main.java

--- java/aviao/view/Main.java
+++ java/aviao/view/Main.java
@@ -7,14 +7,48 @@ public class Main {
       // System.out.println("Hello World !!");
      
       // Etapa 1	   
-      Aviao aviao = new Aviao(); 
+      Aviao aviao = new Aviao("Tesla", "infinit"); 
       aviao.imprimirOk();
 
       // Etapa 2
       //System.out.println("Modelo: " + aviao.getModelo() + " Identificador: " + aviao.getIdentificador());
-      addAviao(aviao, "wall", "goku", true, 1000);
+      //addAviao(aviao, "wall", "goku", true, 1000);
       printAviao(aviao);
-
+      
+      //Etapa4 -> check motor
+      aviao.ligarMotor();
+      aviao.imprimeEstadoMotor();
+      aviao.desligarMotor();
+      aviao.imprimeEstadoMotor();
+      
+      //Etapa5
+      aviao.ligarMotor();
+      aviao.acelerar();
+      aviao.acelerar();
+      aviao.desligarMotor();
+      aviao.acelerar();
+      
+      aviao.ligarMotor();
+      aviao.desacelerar();
+      aviao.desacelerar();
+      aviao.desacelerar();
+      aviao.desligarMotor();
+      aviao.desacelerar();
+      
+      //Etepa6
+      aviao.ligarMotor();
+      aviao.acelerar();
+      aviao.acelerar();
+      aviao.acelerar();
+      aviao.acelerar();
+      aviao.acelerar();
+      
+      aviao.desacelerar();
+      aviao.desacelerar();
+      aviao.desacelerar();
+      aviao.desacelerar();
+      aviao.desacelerar();
+      aviao.desacelerar();
     }
 
    public static void printAviao( Aviao aviao ){
@@ -25,10 +59,10 @@ public class Main {
    }
 
    public static void addAviao( Aviao aviao, String modelo, String id, boolean motor, float altura ){
-   	aviao.setModelo(modelo);
-        aviao.setIdentificador(id);
-        aviao.setMotor(motor);
-        aviao.setAltura(altura);
+       aviao.setModelo(modelo);
+       aviao.setIdentificador(id);
+       aviao.setMotor(motor);
+       aviao.setAltura(altura);
    }
 
 }
